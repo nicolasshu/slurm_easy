d647843e24176b74de14a8c6a84001ff95fdedb1
NickShu <nicolas.s.shu@gmail.com>
2020-06-08 23:55:18 -0400

    Finished

--- v2/create_and_run.sh
+++ v2/create_and_run.sh
@@ -1,13 +1,24 @@
-echo "Begin"
+echo " 1) Begin"
+date
+
 file=""
-env="../envs/speech"
+env="/home/nshu/envs/speech"
 comment=""
+
+cwd=$(pwd)
+
+# PLEASE CHANGE THIS ======================
 email="nshu@emory.edu"
+# =========================================
+
 mailtype="ALL"
 verbose=false
 time="5-23:0:0"
 cp_data=""
 dir="/tmp/nshu/tests"
+
+echo " 2) Parse arguments"
+date
 
 # Parse all of the arguments
 while [ ! $# -eq 0 ]
@@ -17,15 +28,15 @@ do
 			echo "Help"
 			echo "-f | --file         Path to Python file"
 			echo "-e | --env          Path to Python environment"
-			echo "                        (Optional) Default '../envs/speech'"
+			echo "                        (Optional) Default $env"
 			echo "-c | --comment      Custom comment"
 			echo "                        (Optional) Default ''"
 			echo "--email             Email recipient"
-			echo "                        (Optional) Default nshu@emory.edu"
+			echo "                        (Optional) Default $email"
 			echo "--mail-type         Type of alerts on email"
-			echo "                        (Optional) Default: ALL"
+			echo "                        (Optional) Default: $mailtype"
 			echo "-v | --verbose      Verbose boolean"
-			echo "                        (Optional) Default false"
+			echo "                        (Optional) Default $verbose"
 			echo "--copy-data         Copy a specific dataset"
 			echo "-d | --dir          Location of experiment"
 			exit
@@ -57,7 +68,7 @@ do
 			time=$2
 			;;
 		--dir | -d)
-			dir=$2
+			dir="/tmp/nshu/$2"
 			;;
 		--copy-data)
 			cp_data="cp -r /home/data/$2 /tmp/nshu/data"
@@ -86,12 +97,24 @@ then
 	comment="${name}"
 fi
 
+# Create a folder to copy contents back to
+mkdir -p ./results
+
+echo " 3) Create results folder"
+date
+
 # Create the main script
 eval "cat << EOF 
 source ${env}/bin/activate
+echo '18) Activated the environment'
+date
 python ${file}
+echo '19) Ran the python script'
+date
 EOF" > wrapper.sh
 
+echo " 4) Created wrapper.sh"
+date
 
 eval 'cat << EOF
 #!/bin/sh
@@ -107,34 +130,73 @@ eval 'cat << EOF
 #SBATCH --nodes 1
 
 #SBATCH --mail-user=${email}
-#SBATCH --mail-type=${mail-type}
+#SBATCH --mail-type=${mailtype}
+
+# ----------- CUSTOM COPYING BY NICOLAS SHU ----------------
+
+echo " 8) Established the SBATCH parameters"
+date
 
 # Make dirs only if needed
 mkdir -p ${dir}
+echo " 9) Created ${dir}"
+date
+
 mkdir -p /tmp/nshu/data
+echo "10) Created /tmp/nshu/data"
+date
 
 # Copy Data (Optional)
 ${cp_data}
+echo "11) Copied the data"
+date
 
 # Copy Python and Wrapper file to /tmp
 cp $file $dir
+echo "12) Copied $file to $dir"
+date
 cp wrapper.sh $dir
+echo "13) Copied wrapper.sh to $dir"
+date
 
 # Change to that directory
 cd $dir
+echo "14) Changed the directory"
+date
+
+# Copy environment
+# cp -r ${env} ${dir}/../
+
+# Copy gits
+cp -r /home/nshu/basegits/mci /tmp/nshu/
+cp -r /home/nshu/basegits/dataset_loaders /tmp/nshu/
+echo "15) Copied the basegits"
+date
 
 # Make Symbolic Links
 ln -s /tmp/nshu/data $dir 
-ln -s /tmp/nshu/data_loaders/audioloaders $dir
+ln -s /tmp/nshu/dataset_loaders/audioloaders $dir
 ln -s /tmp/nshu/mci/net $dir
+echo "16) Created symbolic links"
+date
+
+# -----------------------------------------------------------
 
 # Run the wrapped Python file
+echo "17) Run scl enable rh-python36"
+date
 scl enable rh-python36 "bash wrapper.sh $file"
 
 # Copy Back
+echo "20) Copying results back"
+date
+cp -r $dir $cwd/results
+
 
 EOF' > ${name}.sh
 
+echo ' 5) Created ${name}.sh'
+date
 
 # Create the wrapper file
 if [ "$verbose" = true ]
@@ -148,10 +210,20 @@ then
 	echo "wrapper.sh"
 	echo "===================="
 	cat wrapper.sh
+	echo " "
 fi 
 
+echo " 6) Show scripts"
+date
 
+echo " 7) Deploy job via sbatch"
+date
+sbatch ${name}.sh
 
+sleep 30
+echo "21) Removing shell scripts"
+date
 # Clean up
-#rm ${name}.sh
-#rm wrapper.sh
+rm ${name}.sh
+rm wrapper.sh
+
